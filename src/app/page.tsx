d8ea937480a6c9f81cdac8fdc039ccb3cd0afbb3
MiroslavSapunar <psapunar@fi.uba.ar>
2024-08-24 02:00:02 -0300

    refactor page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
-import Image from "next/image";
+import dynamic from 'next/dynamic'
+
 import Landing from "./components/landing";
-import Proposals from "./components/proposals";
-import Achievements from "./components/achievements";
+import NavBar from "./components/navbar";
+
+const Elections = dynamic(() => import('./components/elections'), { ssr: false })
+const AboutUs = dynamic(() => import('./components/aboutUs'), { ssr: false })
+const Achievements = dynamic(() => import('./components/achievements'), { ssr: false })
+const Proposals = dynamic(() => import('./components/proposals'), { ssr: false })
 
 export default function Home() {
   return (
     <>
+      {/* <NavBar/> */}
       <Landing />
       <Achievements />
       <Proposals />
+      <Elections />
+      {/* <Elections/> */}
+      <AboutUs/>
     </>
   )
 }
