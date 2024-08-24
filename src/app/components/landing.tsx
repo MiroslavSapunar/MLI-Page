44d9f396d4ef416b7af6f3ebbc2696ebbb4c11bb
MiroslavSapunar <psapunar@fi.uba.ar>
2024-08-24 02:01:21 -0300

    landing

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -1,13 +1,9 @@
 
+import Image from "next/image";
+
 export default function Landing() {
     return (
         <>
-            {/*
-    Heads up! 👋
-  
-    This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
-  */}
-
             <section
                 className='relative bg-[url(/Portada.png)] bg-cover bg-center bg-no-repeat'
             >
@@ -15,45 +11,38 @@ export default function Landing() {
                     className="
                         absolute
                         inset-0 
-                        bg-gray-900/85 
-                        md:bg-gray-900/25
-                        from-gray-900/85 to-gray-900/25
+                        bg-primary/55 
+                        md:bg-primary/25
+                        from-primary/75 to-primary/25
                         bg-gradient-to-r"
                 ></div>
 
                 <div
-                    className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+                    className="relative mx-auto max-w-screen-xl px-4 py-24 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
                 >
-                    <div className="max-w-xl text-left ltr:sm:text-left rtl:sm:text-right">
-                        <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
-                            Movimiento
-
-                            <strong className="block font-extrabold text-rose-500"> Linealmente. </strong>
-                            <strong className="block font-extrabold text-rose-500"> Independiente. </strong>
+                    <div className="max-w-xl text-left">
+                        <h1 className="text-3xl font-extrabold  sm:text-5xl">
+                            <strong className="block font-extrabold text-white"> Movimiento </strong>
+                            <strong className="block font-extrabold text-white"> Linealmente </strong>
+                            <strong className="block font-extrabold text-secundary"> Independiente </strong>
                         </h1>
 
-                        <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
-                            Somos la Presidencia de Centro de Estudiantes de Ingeniería de la UBA y Consejeros por el claustro de Estudiantiles. 
+                        <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed font-roboto ">
+                            En 2004, un grupo de fiubenses, hartos de los problemas recurrentes en sus cursadas decidieron no esperar más a que las soluciones cayerán del cielo.
                         </p>
-                        <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
-                            Desde 2004 construyendo la Ingeniería de la UBA y la Argentina.
+                        {/* <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed font-roboto ">
+                            Siempre con un vision centrada en FIUBA, cada paso que dimos nos permitio <span className="font-bold text-secundary">
+                            construir una mejor FIUBA y la Ingeniería del futuro
+                            </span>en la Argentina.
+                        </p> */}
+                        <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed font-roboto">
+                            Hoy como <span className="font-bold text-secundary"> estudiantes, docentes y graduados</span>, seguimos tranformando nuestra Facultad y carreras, para que cada día se egresen más ingenieros en la Argentina.
+
                         </p>
-
-                        {/* <div className="mt-8 flex flex-wrap gap-4 text-center">
-                            <a
-                                href="#"
-                                className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
-                            >
-                                Get Started
-                            </a>
-
-                            <a
-                                href="#"
-                                className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
-                            >
-                                Learn More
-                            </a>
-                        </div> */}
+                        <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed font-roboto">
+                            Llevamos con orgullo la responsabilidad de ser la voz estudiantil como<span className="font-bold text-secundary"> Presidencia del CEI</span> y <span className="font-bold text-secundary">Consejeros Estudiantiles
+                            </span>
+                        </p>
                     </div>
                 </div>
             </section>
