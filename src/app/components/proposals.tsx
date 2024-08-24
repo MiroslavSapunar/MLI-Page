09bc41b92c2a2188bf4f7442dea345725de474b8
MiroslavSapunar <psapunar@fi.uba.ar>
2024-08-24 02:00:40 -0300

    propuestas

--- src/app/components/proposals.tsx
+++ src/app/components/proposals.tsx
@@ -1,6 +1,6 @@
 export default function Proposals() {
     return (
-        <section className="bg-gray-900 text-white">
+        <section className="bg-secundary text-primary">
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
                 <div className="max-w-xl">
                     <h2 className="text-3xl font-bold sm:text-4xl">Nuestras Propuestas</h2>
