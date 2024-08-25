16b93bb7b1a789831ee334bfc4e3f98e9413c6be
MiroslavSapunar <psapunar@fi.uba.ar>
2024-08-25 01:11:00 -0300

    refactor landing

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -4,21 +4,28 @@ import Image from "next/image";
 export default function Landing() {
     return (
         <>
-            <section
-                className='relative bg-[url(/Portada.png)] bg-cover bg-center bg-no-repeat'
-            >
+            <div className="relative md:min-h-screen">
+                <Image
+                    src='/Portada.png'
+                    alt="portada"
+                    quality={80}
+                    layout="fill"
+                    objectFit="cover"
+                />
                 <div
                     className="
                         absolute
-                        inset-0 
-                        bg-primary/55 
+                        inset-0
+                        bg-primary/55
                         md:bg-primary/25
-                        from-primary/75 to-primary/25
+                        from-primary/75
+                        to-primary/25
                         bg-gradient-to-r"
-                ></div>
+                >
 
+                </div>
                 <div
-                    className="relative mx-auto max-w-screen-xl px-4 py-24 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+                    className="relative mx-auto max-w-screen-xl px-4 pt-24 pb-6 sm:px-6 lg:flex lg:items-center lg:px-8"
                 >
                     <div className="max-w-xl text-left">
                         <h1 className="text-3xl font-extrabold  sm:text-5xl">
@@ -30,11 +37,6 @@ export default function Landing() {
                         <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed font-roboto ">
                             En 2004, un grupo de fiubenses, hartos de los problemas recurrentes en sus cursadas decidieron no esperar más a que las soluciones cayerán del cielo.
                         </p>
-                        {/* <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed font-roboto ">
-                            Siempre con un vision centrada en FIUBA, cada paso que dimos nos permitio <span className="font-bold text-secundary">
-                            construir una mejor FIUBA y la Ingeniería del futuro
-                            </span>en la Argentina.
-                        </p> */}
                         <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed font-roboto">
                             Hoy como <span className="font-bold text-secundary"> estudiantes, docentes y graduados</span>, seguimos tranformando nuestra Facultad y carreras, para que cada día se egresen más ingenieros en la Argentina.
 
@@ -45,7 +47,7 @@ export default function Landing() {
                         </p>
                     </div>
                 </div>
-            </section>
+            </div>
         </>
     )
 }
