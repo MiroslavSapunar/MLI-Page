1222124714ac7b8802f3c3122b57b408cf28e7cc
MiroslavSapunar <psapunar@fi.uba.ar>
2026-03-16 23:27:32 -0300

    updated assets cache time on next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: async () => [
+    {
+      source: '/(.*)\\.(svg|png|jpg|jpeg|webp|ico|woff|woff2)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, immutable',
+        },
+      ],
+    },
+  ],
+};
 
 export default nextConfig;
