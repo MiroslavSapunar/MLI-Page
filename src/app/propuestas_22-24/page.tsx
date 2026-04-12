39175ae872ddd6d4d25adc063878a330fa23861c
MiroslavSapunar <psapunar@fi.uba.ar>
2026-04-12 13:05:37 -0300

    refactor old campaing 22-24

---
+++ src/app/propuestas_22-24/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import NavBar from "../components/navbar"
+import Proposals from '../components/proposals'
+
+export default function PropuestasPage() {
+    return (
+        <div>
+            <NavBar />
+            <Proposals />
+        </div>
+    )
+}
